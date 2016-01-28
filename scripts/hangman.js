8df37ecf4863e65b7ad52bafa6cc3fdcfb0d905f
jwu130 <jwu3725@gmail.com>
2016-01-27 23:59:31 -0500

    Restructured game logic

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -30,72 +30,91 @@ $(document).ready(
 // Keypress handler
 function keypress(e){
 	var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
-	key = String.fromCharCode(key).toUpperCase();
+	key = String.fromCharCode(key).toLowerCase();
 
-	console.log(key);
-	// if(/\d/.exec(e.key))
-	//console.log(word.length);
-	for(i=0; i<word.length; i++){
-		if(word[i] == key){
-           	underscores[i] = key;
-		   	console.log("aqui " + key + " array: " + underscores[i]);
-			$("#word").html(underscores);
-			lettersFound++;
-        } 
-	}
-	
-	if (wrongGuesses < 6) {
-	if(/^[a-zA-Z]$/.exec(key))
-		if(chosenLetters.indexOf(key)<0){
-			$("#keypress").append(key +" ");
-			chosenLetters.push(key);
-			$("#status").html(" ");
-		} else {
-			$("#status").html("You already tried this letter: " + key);
+	// If game is still playing
+	if (checkStatus()) {
+		// If the key pressed is a letter
+		if(/^[a-zA-Z]$/.exec(key)){
+			// If user has not chosen the letter already
+			if(chosenLetters.indexOf(key)<0){
+				// Get rid of status
+				$("#status").html("");
+				// Add to list of chosen letters
+				chosenLetters.push(key);
+				// Display the letter chosen
+				$("#keypress").append(key +" ");
+				// Display letter on the screen with underscores
+				$("#word").html(underscores);
+				if(!checkForLetter(key)){
+					wrongGuesses++;
+					displayBodyPart(wrongGuesses);
+					$("#status").html(key + " is not a letter in this word");
+				}
+			} else {
+				// The user has chosen the letter already
+				$("#status").html("You already tried this letter: " + key);
+			}
 		}
 	}
+	updateStatus();
+};
 
-	checkStatus();
+function checkForLetter(key){
+	var check = false;
+	var count = 0;
+	for(i=0; i<word.length; i++){
+		if(word[i] == key){
+           	underscores[i] = key.toUpperCase();
+           	$("#word-container").html(underscores);
+           	check = true;
+           	count++;
+        } 
+	}
+	lettersFound+=count;
+	return check;
+};
+
+function updateStatus(){
+	console.log(wrongGuesses);
+	if(lettersFound === word.length) {
+		$("#status").html("Congraduations. You won.");
+	} else if (wrongGuesses >= 6) {
+		$("#status").html("You lose... Click restart to begin again");
+	}
+	return true;
 };
 
 function displayBodyPart(){
 	switch (wrongGuesses) {
 		case 1:
-			$(".head").css("visibility", "visible");
+			 $("#wrapper").attr("src","resources/2.png"); 
 			break;
 		case 2:
-			$(".body").css("visibility", "visible");
+			 $("#wrapper").attr("src","resources/3.png"); 
 			break;
 		case 3:
-			$(".left-arm").css("visibility", "visible");
-			break;
+			 $("#wrapper").attr("src","resources/4.png");
+			 break; 
 		case 4:
-			$(".right-arm").css("visibility", "visible");
-			break;
+			 $("#wrapper").attr("src","resources/5.png");
+			 break; 
 		case 5:
-			$(".left-leg").css("visibility", "visible");
-			break;
+			 $("#wrapper").attr("src","resources/6.png");
+			 break; 
 		case 6:
-			$(".right-leg").css("visibility", "visible");
-			break;
+			 $("#wrapper").attr("src","resources/7.png");
+			 break; 
 	}
 	
-}
+};
 
 // Check if the game is over
 function checkStatus(){
-	console.log(lettersFound + " guesses " + wrongGuesses);
-	if(wrongGuesses>6){
-		$("#status").html("You lost...");
+	if(wrongGuesses >= 6 || lettersFound === word.length){
+		return false
 	}
-	else if(lettersFound === word.length){
-		$("#status").html("Congraduations. You won.");
-		// Also hault the handling of key presses?
-	}
-	else {
-        	wrongGuesses++;
-        	displayBodyPart();
-        }
+	return true;
 };
 
 // Restart game
@@ -103,7 +122,13 @@ function restart(){
 	//set guesses to 0 for new game
 	wrongGuesses = 0;
 
+	// Reset displays
+	$("#word-container").html("");
+	$("#status").html("");
+	$("#wrapper").attr("src","resources/1.png"); 
+
 	// Retrieve new word
+	underscores = [];
 	word = getNewWord();
 	generateDashes(word);
 
@@ -122,12 +147,14 @@ function restart(){
 
 // Returns a new random word from array
 function getNewWord(){
-	var index = Math.floor(Math.random() * (8-0));
+	var index = Math.floor(Math.random() * (7-0));
+	console.log(index);
 	var wordArr = words[index].split("");
 	$("#description").html(descriptions[index]);
 	return wordArr;
-}
+};
 
+// Generate the word to be displayed with underscores
 function generateDashes(word){
 	
 	var wordLength = word.length;
